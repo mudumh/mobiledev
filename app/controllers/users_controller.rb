429dd173e81927ba13ce9df85629f2b90a447586
harsha <harsha.jaigurudev@gmail.com>
2014-12-18 11:28:42 -0700

    user profile feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @tools_upvoted = @user.get_up_voted Tool
     @news_upvoted = @user.get_up_voted NewsItem
-    
     @tutorials_upvoted = @user.get_up_voted Tutorial
-    
+    if params[:s] == 'comments'
+      @comments = @user.comments
+    end
   end
 
   def index
