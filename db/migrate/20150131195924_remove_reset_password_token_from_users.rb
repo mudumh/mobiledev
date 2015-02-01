0b0852922c6010c3faadac1c43749f701192cd2e
harsha <harsha.jaigurudev@gmail.com>
2015-01-31 22:38:18 -0500

    bug fix

--- db/migrate/20150131195924_remove_reset_password_token_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveResetPasswordTokenFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :reset_password_token
-    remove_column :users, :reset_password_sent_at
-  end
-end
--- db/migrate/20150131200921_add_columns_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :reset_password_token, :string
-  end
-end
